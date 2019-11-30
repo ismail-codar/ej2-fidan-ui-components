613b1f7e244566b67c6a54460aea51d1277a4fee
ismail codar <icodar@gmail.com>
2019-11-30 06:51:34 +0300

    Sidebar pozisyonunu hatırlama

--- src/pages/sidebar/AppSidebarLayout.tsx
+++ src/pages/sidebar/AppSidebarLayout.tsx
@@ -1,10 +1,11 @@
-import { Sidebar } from "@syncfusion/ej2-navigations";
+import { Sidebar, EventArgs } from "@syncfusion/ej2-navigations";
 import { AppSelectedProperties } from "../../components/sidebar-layout/AppSelectedProperties";
 import { SfSidebar } from "../../ej2-fidan-ui-components/Sidebar";
 import { SfSplitter } from "../../ej2-fidan-ui-components/Splitter";
 import { SfTab } from "../../ej2-fidan-ui-components/Tab";
 import { AppTreeView } from "../../components/sidebar-layout/AppTreeView";
 import { FormDemo } from "../../components/form/_.demo";
+import { EmitType } from "@syncfusion/ej2-base";
 
 export const AppSidebarLayout = () => {
   let sideBar: Sidebar = null;
@@ -17,12 +18,23 @@ export const AppSidebarLayout = () => {
           <span
             id="hamburger"
             className="e-icons menu default"
-            onClick={() => sideBar.show()}
+            onClick={() => {
+              sideBar.show();
+            }}
           />
           <SfSidebar
             title=" AppId "
             onInit={props => {
               sideBar = props._component;
+              sideBar.addEventListener("open", (e: EmitType<EventArgs>) => {
+                localStorage.setItem("sideBar", "1");
+              });
+              sideBar.addEventListener("close", (e: EmitType<EventArgs>) => {
+                localStorage.setItem("sideBar", "0");
+              });
+              if (localStorage.getItem("sideBar") !== "1") {
+                sideBar.hide();
+              }
             }}
           >
             Place your primary content here...
