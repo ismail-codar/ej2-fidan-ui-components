ec70f4b7eec896e507e54fc4c3b4830f4541919c
ismail codar <icodar@gmail.com>
2020-01-01 07:00:09 +0300

    setupComponentView

--- src/_base.ts
+++ src/_base.ts
@@ -1,18 +1,31 @@
-import { FidanValue } from "@fidanjs/runtime";
+import onload from 'on-load';
+import { FidanValue } from '@fidanjs/runtime';
 
 export interface ComponentBase<T> {
-  id?: any;
-  children?: any;
-  _component?: T;
-  _view?: HTMLElement;
-  didMount?(props: ComponentBase<T>);
-  required?: boolean;
+	id?: any;
+	children?: any;
+	_component?: T;
+	_view?: HTMLElement;
+	didMount?(props: ComponentBase<T>);
+	required?: boolean;
 }
 
 export interface InputWithMessageProps<T> extends ComponentBase<T> {
-  id: string;
-  name: string;
-  placeholder?: string;
-  containerId: string;
-  inputValue?: FidanValue<any>
+	id: string;
+	name: string;
+	placeholder?: string;
+	containerId: string;
+	inputValue?: FidanValue<any>;
 }
+
+export const setupComponentView = (view, component) => {
+	onload(
+		view,
+		(el) => {
+			//
+		},
+		(el) => {
+			//
+		}
+	);
+};
