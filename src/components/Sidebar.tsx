39c1b1a7090a0d3c1c1962562f50bbe1d7e06e49
ismail codar <icodar@gmail.com>
2020-01-04 12:59:43 +0300

    Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,22 +1,17 @@
-
-import { Sidebar, SidebarModel } from "@syncfusion/ej2-navigations";
-import { SideBarProps } from "./props/SideBarProps"
+import { Sidebar, SidebarModel } from '@syncfusion/ej2-navigations';
+import { SideBarProps } from './props/SideBarProps';
 import { setupComponentView } from '../_base';
-    
+
 export const SfSidebar = (props: SidebarModel & SideBarProps) => {
-  const _view = (
-      <aside className={props.sidebarCss || "default-sidebar"}>
-        <div className={props.titleHeaderCss || "title-header"}>
-          <div style="display:inline-block"> {props.title} </div>
-          <span
-            id="close"
-            className={props.eIconsCss || "e-icons"}
-            onClick={() => _component.hide()}
-          />
-        </div>
-        <div className={props.subTitleCss || "sub-title"}>{props.children}</div>
-      </aside>
-    );
-  const _component = setupComponentView(_view, props, Sidebar);
-  return _view;
+	const _view = (
+		<aside className={props.sidebarCss || 'sidebar-container'}>
+			<div className={props.titleHeaderCss || 'sidebar-title-header'}>
+				<div style="display:inline-block"> {props.title} </div>
+				<span id="close" className={props.eIconsCss || 'e-icons'} onClick={() => _component.hide()} />
+			</div>
+			<div className={props.subTitleCss || 'sidebar-content'}>{props.children}</div>
+		</aside>
+	);
+	const _component = setupComponentView(_view, props, Sidebar);
+	return _view;
 };
